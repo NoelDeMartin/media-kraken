1415aa220f4fa0640c4c1628b7516f4005f726d5
Noel De Martin <noeldemartin@gmail.com>
2021-01-30 12:33:25 +0100

    Fix clearImmediate polyfill

--- src/utils/polyfills.ts
+++ src/utils/polyfills.ts
@@ -1,4 +1,4 @@
 export function setUpPolyfills(globalObject: any) {
     globalObject.setImmediate = (callback: Function, ...params: any[]) => setTimeout(() => callback(...params), 0);
-    globalObject.clearImmediate = (callback: Function, ...params: any[]) => setTimeout(() => callback(...params), 0);
+    globalObject.clearImmediate = (...params: any[]) => clearTimeout(...params);
 }
