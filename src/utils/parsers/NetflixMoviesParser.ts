1f421fc3043c4841fac5c8ef1c41d122a79b3f10
Noel De Martin <noel@noeldemartin.com>
2024-12-03 10:37:03 +0800

    #28 Improve importing from Netflix

--- src/utils/parsers/NetflixMoviesParser.ts
+++ src/utils/parsers/NetflixMoviesParser.ts
@@ -1,10 +1,12 @@
-import TheMovieDBApi from '@/api/TheMovieDBApi';
+import TheMovieDBApi, { TMDBMovie } from '@/api/TheMovieDBApi';
 
 import MediaValidationError from '@/errors/MediaValidationError';
+import UnsuitableMediaError from '@/errors/UnsuitableMediaError';
 
 import Movie from '@/models/soukai/Movie';
 
 import { MediaParser } from '@/utils/parsers';
+import Str from '@/utils/Str';
 import TMDBMoviesParser from '@/utils/parsers/TMDBMoviesParser';
 
 interface Data {
@@ -12,22 +14,38 @@ interface Data {
     date: string;
 }
 
+const SHOW_TITLE_REGEX = /season\s+\d+:/i;
+const matches: WeakMap<Data, { match: TMDBMovie | null }> = new WeakMap();
+
 class NetflixMoviesParser implements MediaParser<Data, Movie> {
 
     public async validate(data: Data): Promise<void> {
-        if (typeof data.title !== 'string' || data.title.length < 3)
+        if (typeof data.title !== 'string' || data.title.length < 3) {
             throw new MediaValidationError(['Invalid format']);
+        }
 
-        if (typeof data.date !== 'string' || isNaN(new Date(data.date) as unknown as number))
+        if (typeof data.date !== 'string' || isNaN(new Date(data.date) as unknown as number)) {
             throw new MediaValidationError(['Invalid format']);
+        }
+
+        if (SHOW_TITLE_REGEX.test(data.title)) {
+            throw new UnsuitableMediaError('Not a movie');
+        }
+
+        if (!(await this.findMovie(data))) {
+            throw new MediaValidationError(['This movie could not be found']);
+        }
     }
 
     public async parse(data: Data): Promise<Movie> {
         const watchedAt = new Date(data.date);
-        const { results } = await TheMovieDBApi.searchMovies(data.title);
-        const match = results.find(result => result.release_date && (new Date(result.release_date) < watchedAt))
-                    ?? results[0];
-        const movie = match ? await TMDBMoviesParser.parse(match) : new Movie({ title: data.title });
+        const match = await this.findMovie(data);
+
+        if (!match) {
+            throw new Error('Movie data didn\'t have a match');
+        }
+
+        const movie = await TMDBMoviesParser.parse(match);
 
         movie.createdAt = watchedAt;
         movie.updatedAt = watchedAt;
@@ -36,6 +54,27 @@ class NetflixMoviesParser implements MediaParser<Data, Movie> {
         movie.watch(watchedAt);
 
         return movie;
+    }
+
+    private async findMovie(data: Data): Promise<TMDBMovie | null> {
+        if (matches.has(data)) {
+            console.log('use cache!');
+
+            return matches.get(data)?.match ?? null;
+        }
+
+        const watchedAt = new Date(data.date);
+        const slug = Str.slug(data.title);
+        const { results } = await TheMovieDBApi.searchMovies(data.title);
+        const match = results.find(result =>
+            result.release_date &&
+            (new Date(result.release_date) < watchedAt) &&
+            Str.slug(result.title) === slug,
+        ) ?? null;
+
+        matches.set(data, { match });
+
+        return match;
     }
 
 }
